07c6564a9735113ab781973add93ad4e6dc78ad8
francosalvucci14 <francosalvucci14@gmail.com>
2024-05-13 11:13:09 +0200

    Commit

---
+++ JavaScript/JS-Asincrono/EsercizioFetchCane/script.js
@@ -0,0 +1,13 @@
+function caricaImmagine() {
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(data => {
+        const imageUrl = data.message;
+        const dogImage = document.createElement('img');
+        dogImage.src = imageUrl;
+        dogImage.alt = 'Immagine di Cane';
+        document.getElementById('dog-image').innerHTML = '';
+        document.getElementById('dog-image').appendChild(dogImage);
+    })
+    .catch(error => console.error('Si è verificato un errore:', error));
+}
